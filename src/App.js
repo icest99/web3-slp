5d90248723804833b943b67426163e2f9cfaf84c
Kiattipan Muangyai <57340445+icest99@users.noreply.github.com>
2021-11-28 19:32:37 +0700

    UI update.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Axios from "axios";
 import { useState, useEffect } from 'react'
 import Web3 from 'web3';
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import abi from './contractABI';
+import { makeStyles } from "@material-ui/core/styles"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import {
+  Drawer, List, ListItem,
+  ListItemIcon, ListItemText,
+  Container, Typography,
+} from "@material-ui/core";
+import { grey } from '@mui/material/colors';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import AddIcon from '@mui/icons-material/Add';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import PercentIcon from '@mui/icons-material/Percent';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import TransitEnterexitIcon from '@mui/icons-material/TransitEnterexit';
+
+
+const useStyles = makeStyles((theme) => ({
+  drawerPaper: { width: 'inherit'},
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  }
+}))
+
 
 function App() {
+    const classes = useStyles();
 
   const [roninAddress, setRoninAddress] = useState('')
   const [playerAddress, setPlayerAddress] = useState('')
@@ -19,6 +46,11 @@ function App() {
   const [weiAmount, setWeiAmount] = useState('')
   const [scholarUint, setScholarUint] = useState('')
   const [guildMasterAddress, setGuildMasterAddress] = useState('')
+  const [balance, setBalance] = useState('')
+  const [totalClaimAble, setTotalClaimAble] = useState('')
+  const [axieName, setAxieName] = useState('')
+  const [axieSLP, setaxieSLP] = useState('')
+
 
   async function loadWeb3() {
     if (window.ethereum) {
@@ -27,107 +59,93 @@ function App() {
     }
   }
 
-  // var account = web3.eth.accounts.wallet;
-  // console.log("my address:", account);
+    //Run once, to get current wallet address/guildMaster
+    useEffect(() => {
+        getAccounts();
+        callGuildMaster();
+        callBalance();
+        callClaimableTotal();
+        }, [])
+
+    const getAxieAPI = () => {
+        Axios.get("https://game-api.axie.technology/api/v1/ronin:" + roninAddress).then(
+            (response) => {
+                setAxieName(response.data.name);
+                setaxieSLP(response.data.total_slp);
+            }
+        )
+    }
+
+    useEffect(() => {
+        getAxieAPI();
+    }, [])
 
   async function loadContract() {
       return await new window.web3.eth.Contract(
           abi
-          , '0x33085Ca77b74089F30835F79E010E35F50ce423A');
+          , '0x2a86Cedf01e10AF7E0EBA20029D12EC3D4cfb8ff');
   }
   
 
   //Function Call
   async function callScholar() {
-    // updateStatus('Checking Scholar Numbers...');
+
         await  window.contract.methods.scholarLength().call()
             .then(res => console.log('number of scholar:', res))
             .catch(err => console.log(err))
-    // updateStatus(`Checking Done!`);
-  }
 
-  async function callClaimableTotal() {
-    // updateStatus('Checking Scholar Numbers...');
-        await  window.contract.methods.claimableTotal().call()
-            .then(res => console.log('claimableTotal:', res))
-            .catch(err => console.log(err))
-    // updateStatus(`Checking Done!`);
   }
-
-  async function callBalance() {
-    // updateStatus('Checking Scholar Numbers...');
-        await  window.contract.methods.balance().call()
-            .then(res => console.log('Balance:', res))
-            .catch(err => console.log(err))
-    // updateStatus(`Checking Done!`);
-  }  
 
   async function callPlayerList() {
-    // updateStatus('Checking Scholar Numbers...');
+
         await  window.contract.methods.playerList(playerAddress).call()
             .then(res => console.log('PlayerList:', res))
             .catch(err => console.log(err))
-    // updateStatus(`Checking Done!`);
+
   }  
 
   async function callRoninInfo() {
-    // updateStatus('Checking Scholar Numbers...');
         await  window.contract.methods.roninInfo(roninAddress).call()
             .then(res => console.log('roninInfo:', res))
             .catch(err => console.log(err))
-    // updateStatus(`Checking Done!`);
   }
 
   async function callScholarInfo() {
-    // updateStatus('Checking Scholar Numbers...');
         await  window.contract.methods.scholarInfo(roninAddress).call()
             .then(res => console.log('Scholar Info:', res))
             .catch(err => console.log(err))
-    // updateStatus(`Checking Done!`);
   }  
-
-    // scholarInfo
-
-//------------------------------
-//------------------------------
-
 
   
   //Function Send
 
-  //ADD TARGET WALLET
   async function addScholar() {
-      // updateStatus('Add Scholar...');
       await  window.contract.methods.addScholar(roninAddress,
       roninAddress, percentShare).send({from: currentAccount})
   }
 
   async function changePlayer() {
-      // updateStatus('Changing Player...');
       await  window.contract.methods.changePlayer(roninAddress,
       playerAddress).send({from: currentAccount})
-      // updateStatus('Changed player successfully!')
   }
 
   async function removeOldPlayer() {
-      // updateStatus('Removing Old Player..');
       await  window.contract.methods.removeOldPlayer(playerAddress).send({from: currentAccount})
-      // updateStatus('Deposit successfully!')
   }
 
   async function deposit() {
-      // updateStatus('deposit');
+     
       await  window.contract.methods.deposit(weiAmount).send({from: currentAccount})
   }
 
   async function withdraw() {
-    // updateStatus('deposit');
+   
     await  window.contract.methods.withdraw(weiAmount).send({from: currentAccount})
 }
 
   async function updatePercentShare() {
       // updateStatus('Updating Percent Share...');
-      await  window.contract.methods.updatePercentShare(currentAccount, percentShare).send({from: currentAccount})
+      await  window.contract.methods.updatePercentShare(roninAddress, percentShare).send({from: currentAccount})
   }
 
 
@@ -141,32 +159,34 @@ function App() {
   async function load() {
       await loadWeb3();
       window.contract = await loadContract();
-      // updateStatus('Ready!');
+
   }
 
-  //the update function, just just to change text. to 'Readt!'
-  // function updateStatus(status) {
-  //     const statusEl = document.getElementById('status');
-  //     statusEl.innerHTML = status;
-  //     console.log(status);
-  // }
 
   load();
 
+  async function callBalance() {
 
-  //Run once, to get current wallet address/guildMaster
-  useEffect(() => {
-    getAccounts();
-    callGuildMaster();
-  }, [])
-
-  async function callGuildMaster() {
-
-        await  window.contract.methods.guildMaster().call()
-            .then(res => setGuildMasterAddress(res))
+        await  window.contract.methods.balance().call()
+            .then(res => setBalance(res))
             .catch(err => console.log(err))
 
   }
+
+  async function callClaimableTotal() {
+
+        await  window.contract.methods.claimableTotal().call()
+            .then(res => setTotalClaimAble(res))
+            .catch(err => console.log(err))
+
+  }
+
+    async function callGuildMaster() {
+    await  window.contract.methods.guildMaster().call()
+        .then(res => setGuildMasterAddress(res))
+        .catch(err => console.log(err))
+
+    }
 
   function getAccounts() {
     window.web3.eth.getAccounts((error,result) => {
@@ -179,90 +199,213 @@ function App() {
     });
   }
 
-  //print current selected account
-  // console.log("test", currentAccount);
-  // console.log("what is it", typeof(currentAccount), currentAccount)
 
-  return (
-    <div className="App">
-
-        <label>
-          <span>Ronin Address:</span>
-          <input 
+  const roninAddressInnput = <label>
+        <span>Ronin Address:</span>
+        <input 
             type="text" 
             onChange={(e) => setRoninAddress(e.target.value)}
             value={roninAddress}
-          />
+        />
         </label>
-        <br/>
-        <label>
-          <span>Player Address:</span>
-          <input 
+
+  const playerAddressInput = <label>
+    <span>Player Address:</span>
+        <input 
             type="text" 
             onChange={(e) => setPlayerAddress(e.target.value)} 
             value={playerAddress}
-          />
+        />
         </label>
+
+  const scholarUintInput = <label>
+            <span>ScholarUint:</span>
+            <input 
+                type="text" 
+                onChange={(e) => setScholarUint(e.target.value)} 
+                value={scholarUint}
+            />
+            </label>
+
+  const  percentShareInput = <label>
+            <span>Percent Share:</span>
+            <input 
+                type="text" 
+                onChange={(e) => setPercentShare(e.target.value)} 
+                value={percentShare}
+            />
+            </label>
+
+  const weiAmountInput = <label>
+        <span>Deposit:</span>
+        <input 
+        type="text" 
+        onChange={(e) => setWeiAmount(e.target.value)} 
+        value={weiAmount}
+        />
         <br/>
-        <label>
-          <span>ScholarUint:</span>
-          <input 
-            type="text" 
-            onChange={(e) => setScholarUint(e.target.value)} 
-            value={scholarUint}
-          />
         </label>
-        <br/>
-        {(currentAccount === guildMasterAddress) &&
-        <label>
-          <span>Percent Share:</span>
-          <input 
-            type="text" 
-            onChange={(e) => setPercentShare(e.target.value)} 
-            value={percentShare}
-          />
-        </label>}
 
-        {/* <p>roninAddress - {roninAddress}, playerAddress - {playerAddress}, percentShare - {percentShare} </p> */}
+    const weiAmountInputW = <label>
+    <span>Withdraw:</span>
+    <input 
+    type="text" 
+    onChange={(e) => setWeiAmount(e.target.value)} 
+    value={weiAmount}
+    />
+    </label>
 
-        <br/><br/>
+    console.log(roninAddress)
+
+
+  return (
+    <div className="App">
+        <Router>
+        <div style={{ display: 'flex' }}>
+            <Drawer
+            style={{ width: '10rem' }}
+            variant="persistent"
+            anchor="left"
+            open={true}
+            classes={{ paper: classes.drawerPaper }}
+            >
+          <List>
+            <Link to="/" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon/>
+                </ListItemIcon>
+                <ListItemText primary={"Home"} />
+              </ListItem>
+            </Link>
+            {(currentAccount === guildMasterAddress) &&
+            <Link to="/add-scholar" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <AddIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Add Scholar"} />
+              </ListItem>
+            </Link>}
+            {(currentAccount === guildMasterAddress) &&
+            <Link to="/change-player" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <ChangeCircleIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Change Player"} />
+              </ListItem>
+            </Link>}
+            {(currentAccount === guildMasterAddress) &&
+            <Link to="/remove-player" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <PersonRemoveIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Remove Player"} />
+              </ListItem>
+            </Link>}
+            {(currentAccount === guildMasterAddress) &&
+            <Link to="/change-share-percent" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <PercentIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Change Share Percent"} />
+              </ListItem>
+            </Link>}
+            {(currentAccount === guildMasterAddress) &&
+            <Link to="/deposit" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <PaymentsIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Deposit"} />
+              </ListItem>
+            </Link>}
+
+            {(currentAccount === guildMasterAddress) &&
+            <Link to="/withdraw" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <TransitEnterexitIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Withdraw"} />
+              </ListItem>
+            </Link>}
+
+          </List>
+        </Drawer>
+        <Switch>
+        <div className="interfaceUI">
+          <Route exact path="/">
+            <Container>
+            {(roninAddress !== '') &&
+            <React.Fragment>
+                In-game Name: {axieName} <br/> 
+            </React.Fragment>}
+
+            {(roninAddress !== '') &&
+                <React.Fragment>
+                Total SLP: {axieSLP} <br/>
+            </React.Fragment>}
+
+                Pool Balance: {balance} <br/>
+                Claimable Amount: {totalClaimAble}
+            </Container>
+          </Route>
+          <Route exact path="/add-scholar">
+            {(currentAccount === guildMasterAddress) &&<Container>
+                {roninAddressInnput} <br/>
+                {playerAddressInput} <br/>
+                {percentShareInput} <br/>
+                <button className="bn5" onClick={addScholar}>Add Scholar</button>
+            </Container>}
+          </Route>
+          <Route exact path="/change-player">
+          {(currentAccount === guildMasterAddress) && <Container>
+                {roninAddressInnput} <br/>
+                {playerAddressInput} <br/>
+                <button className="bn5" onClick={changePlayer}>Change Player</button>
+            </Container>}
+          </Route>
+          <Route exact path="/remove-player">
+            {(currentAccount === guildMasterAddress) && <Container>
+                {playerAddressInput} <br/>
+                <button className="bn5" onClick={removeOldPlayer}>Remove Old Player</button>
+            </Container>}
+          </Route>
+          <Route exact path="/change-share-percent">
+            {(currentAccount === guildMasterAddress) && <Container>
+                {percentShareInput} <br/>
+                <button className="bn5" onClick={updatePercentShare}>Update Percent Share</button>
+            </Container>}
+          </Route>
+          <Route exact path="/deposit">
+            {(currentAccount === guildMasterAddress) &&<Container>
+                {weiAmountInput}
+                <button className="bn5" onClick={deposit}>Deposit</button>
+                
+            </Container>}
+          </Route>
+          <Route exact path="/withdraw">
+            {(currentAccount === guildMasterAddress) &&<Container>
+                {weiAmountInput}
+                <button className="bn5" onClick={withdraw}>withdraw</button>
+                
+            </Container>}
+          </Route>
+        </div>
+        </Switch>
+      </div>
+    </Router>
         
-        {(currentAccount === guildMasterAddress) &&
-        <button onClick={addScholar}>Add Scholar</button>
-        }
-
-        {(currentAccount === guildMasterAddress) &&
-        <button onClick={changePlayer}>Change Player</button>
-        }
-
-        {(currentAccount === guildMasterAddress) &&
-        <button onClick={removeOldPlayer}>Remove Old Player</button>
-        }
-
-        {(currentAccount === guildMasterAddress) &&
-        <button onClick={updatePercentShare}>Update Percent Share</button>
-        }
-
-        <button onClick={callScholar}>scholarLength</button>
-
-        <br/><br/>
-
-        <label>
-          <span>Deposit:</span>
-          <input 
-            type="text" 
-            onChange={(e) => setWeiAmount(e.target.value)} 
-            value={weiAmount}
-          />
-        </label>
-
-        <br/><br/>
-
-        <button onClick={deposit}>deposit</button>
-        <button onClick={callBalance}>Balance</button>
+        {/* use or not? */}
+        {/* <button onClick={callScholar}>scholarLength</button> */}
+        {/* <button onClick={callBalance}>Balance</button>
         <button onClick={callClaimableTotal}>ClaimableTotal</button>
         <button onClick={callRoninInfo}>Ronin Info</button>
-        <button onClick={callScholarInfo}>Scholar Info</button>
+        <button onClick={callScholarInfo}>Scholar Info</button> */}
 
     </div>
   );
